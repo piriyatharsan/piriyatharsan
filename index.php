9fb6c1e8d2384616dd4866fa9b32d6127eb080f9
piriyatharsan <86178608+piriyatharsan@users.noreply.github.com>
2021-06-20 13:09:50 +0530

    etha paru oruka

--- index.php
+++ index.php
@@ -5,7 +5,108 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>audi car</title>
-    <link rel="stylesheet" href="css/index.css">
+    <style>
+        #topbar{
+    background-color: white;
+    height: 80px;
+    width: 100%;
+}
+#topbar img{
+    height: 80px;
+}
+   
+#topbar ul{
+    float:right;
+    margin-top: 25px;
+
+}
+#topbar ul li{
+    display: inline;
+    margin-right: 40px;
+}
+#topbar ul li a{
+    color: RED;
+    text-decoration: none;
+    font-size: 20px;
+}
+body{
+    margin: 0;
+    padding: 0;
+
+}
+.header{
+    height: 100vh;
+    background-image: url("../image/audicar.jpg") ;
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    font-family: sans-serif;
+
+}
+h1{
+    color: #fff;
+    margin-bottom:70px ;
+    font-size: 45px;
+    letter-spacing: 2px;
+    text-align:center ;
+}
+.Search-field{
+    height: 50px;
+    padding: 10px;
+    border-radius:25px ;
+    outline: none;
+
+}
+.Select Model{
+    width: 400px;
+}
+.Select Type{
+    width: 200px;
+}
+.Search-btn{
+    height: 50px;
+    width: 150px;
+    background: #ffeb3b;
+    border-radius: 25px;
+    outline: none;
+}
+.search-btn:hover{
+  background: #ffc107;
+  cursor: pointer;
+}
+.form-box{
+    background:rgba(0, 0, 0, 0.5) ;
+    padding:30px;
+}
+#footer{
+    width: 100%;
+    height: 50px;
+    background-color: rgb(0, 0, 0);
+    clear: both;
+    bottom:0px;
+    top: 100px;
+    padding-top: 10px;
+    
+  
+    
+}
+#footer p{
+    color:white ;
+    float:right;
+    margin-right: 20px;
+    margin-top: 0px;
+}
+#footer ul li{
+    color:white ;
+    float:left;
+    margin-right: 20px;
+    text-decoration: none;
+    display: inline;
+
+}
+    </style>
 </head>
 <body>
     <div id="topbar">
--- sellcar.php
+++ sellcar.php
@@ -35,24 +35,30 @@ button[type="submit"]:hover{
 
 <label>barnd</label>
 <input type="text" name="barnd" placeholder="brand" required> <br><br>
+
 <label>model</label>
-<input type="text" name="modal" placeholder="model" required><br><br>
+<input type="text" name="model" placeholder="model" required><br><br>
+
 <label>condition</label>
 <input type="text" name="condition" placeholder="condition" required><br><br>
+
 <label>price</label>
 <input type="text" name="price" placeholder="price"  required><br><br>
+
 <label>description</label>
 <input type="text" name="description," placeholder="description"  required><br><br>
+
 <label>image</label>
- <input type="file" name="image" id="image"/>
+<input type="file" name="image" id="image"/>
 <button type="submit" name="upload" value="upload Image/Data">upload</button>
 </form>
 </body>
 </html>
            
 <?php
+error_reporting(0);
 $connection = mysqli_connect("localhost","root","");
-$db = mysqli_select_db($connection,'account');
+$db = mysqli_select_db($connection,'studentdb');
 
 if(isset($_POST['upload']))
 {
@@ -63,17 +69,17 @@ $price = $_POST['price'];
 $description = $_POST['description'];
 $file=addslashes(file_get_contents($_FILES["image"]["tmp_name"]));
 
-$query = "INSERT INTO 'users'('barand','model','condition','price','description','image') VALUES ('$brand','$model','$condition','$price','$description','$file')";
+$query = "INSERT INTO 'carregister'('barnd','model','condition','price','description','image') VALUES ('$brand','$model','$condition','$price','$description','$file')";
 $query_run = mysqli_query($connection,$query);
 
 if($query_run)
 {
-	echo '<script type ="text/javascript">alert("image profile uploaded)</script>';
+	echo '<script type ="text/javascript"> alert("image profile uploaded") </script>';
 
 }
 else
 {
-	echo '<script type ="text/javascript">alert("image profile not uploaded)</script>';
+	echo '<script type ="text/javascript"> alert("image profile not uploaded") </script>';
 }
 
 }
